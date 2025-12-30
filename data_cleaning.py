7e276f4300917db6979d1e0dcb34fc23dccac369
S8ngh <singhswaraj027@gmail.com>
2025-12-30 10:02:18 +0530

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,10 @@
+def clean_data(df):
+    df = df.drop_duplicates()
 
+    for col in df.select_dtypes(include="number").columns:
+        df[col].fillna(df[col].mean(), inplace=True)
+
+    for col in df.select_dtypes(include="object").columns:
+        df[col].fillna("Unknown", inplace=True)
+
+    return df
