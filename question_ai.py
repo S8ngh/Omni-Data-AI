19168b62b9e7ae8cc762290d3928c8d832f26e48
S8ngh <singhswaraj027@gmail.com>
2025-12-30 10:02:53 +0530

    Update question_ai.py

--- question_ai.py
+++ question_ai.py
@@ -1,1 +1,12 @@
+def answer_question(df, question):
+    q = question.lower()
 
+    if "highest" in q:
+        col = df.select_dtypes(include="number").columns[0]
+        return f"Highest {col} is {df[col].max()}"
+
+    if "average" in q:
+        col = df.select_dtypes(include="number").columns[0]
+        return f"Average {col} is {df[col].mean()}"
+
+    return "Question understood, feature coming soon!"
