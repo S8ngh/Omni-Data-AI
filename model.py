e4364f600fa2ffda1554f2f2066f308c4db794b5
S8ngh <singhswaraj027@gmail.com>
2025-12-30 10:05:00 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
+from sklearn.linear_model import LinearRegression
 
+def train_model(df):
+    num = df.select_dtypes(include="number")
+
+    X = num.iloc[:, :-1]
+    y = num.iloc[:, -1]
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    return "Prediction model trained successfully"
